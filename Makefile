982eb5537f23557190124764dc9c71f3a8c86a9c
freshkitedev <95519598+freshkitedev@users.noreply.github.com>
2022-06-05 13:38:55 +0530

    Initial commit

---
+++ Makefile
@@ -0,0 +1,2 @@
+math : sum.c init.c
+	gcc -o math init.c sum.c -I.
---
+++ init.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <maths.h>
+
+
+void main()
+{
+    int a, b;
+    printf("\nMath function are here");
+    a = 15;
+    b = 5;
+    printf("\nSum: %d\n", sum(a,b));
+}
